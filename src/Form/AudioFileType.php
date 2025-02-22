902427dc188c1a3609122969264c4739bbf27fce
Novitz Jean-Philippe <novitz@gmail.com>
2025-02-22 04:42:12 +0100

    Refactor File Form Type
    Add Constraints for file type and file size in form type.
    Add form error widget

--- src/Form/AudioFileType.php
+++ src/Form/AudioFileType.php
@@ -6,16 +6,33 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class AudioFileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('file', FileType::class, [
-            'label'    => 'Télécharger le fichier',
-            'mapped'   => false,
+            'label' => 'Télécharger le fichier',
+            'mapped' => false,
             'required' => true,
-        ]);
+            'constraints' => [
+                new File([
+                    'maxSize' => '204800k',
+                    'mimeTypes' => [
+                        'audio/m4a',
+                        'audio/mpeg',
+                        'audio/mp3',
+                        'audio/wav',
+                        'audio/x-wav',
+                        'audio/wave',
+                    ]
+                ]),
+                new NotBlank([
+                    'message' => 'Veuillez sélectionner un fichier audio',
+                ]),
+            ]]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
