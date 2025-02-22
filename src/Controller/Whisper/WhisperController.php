3498ecda90356296b6853d714422bda62c8228c9
Novitz Jean-Philippe <novitz@gmail.com>
2025-02-22 04:13:48 +0100

    Clean Controller/template
    - WhisperController
    - index template

--- src/Controller/Whisper/WhisperController.php
+++ src/Controller/Whisper/WhisperController.php
@@ -5,23 +5,22 @@ namespace App\Controller\Whisper;
 use App\Form\AudioFileType;
 use App\Message\AudioSplitMessage;
 use App\Message\AudioToTextMessage;
-use App\Message\ConvertAudioMessage;
-use App\Service\AudioConverterService;
-use App\Service\AudioSplitterService;
-use App\Service\WhisperService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class WhisperController extends AbstractController
 {
 
+    /**
+     * @throws ExceptionInterface
+     */
     #[Route('/', name: 'app_default')]
-    public function upload(Request $request, MessageBusInterface $bus,HubInterface $hub): Response
+    public function upload(Request $request, MessageBusInterface $bus, HubInterface $hub): Response
     {
         $form = $this->createForm(AudioFileType::class);
         $form->handleRequest($request);
@@ -29,24 +28,15 @@ final class WhisperController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $file = $form->get('file')->getData();
             if ($file) {
-                // Récupère le chemin de destination depuis les paramètres (voir ci-dessous)
+                // Récupère le chemin de destination depuis les paramètres
                 $destination = $this->getParameter('upload_directory');
                 // Nom fixe pour le fichier uploadé (attention : il sera écrasé à chaque upload)
                 $nomFichier = 'input.' . $file->getClientOriginalExtension();
 
-                try {
-                    $file->move($destination, $nomFichier);
-//                    return $this->redirectToRoute('app_conversion');
-                    $bus->dispatch(new ConvertAudioMessage());
-                    $bus->dispatch(new AudioSplitMessage());
-                    $bus->dispatch(new AudioToTextMessage());
-//die;
-                    return $this->render('whisper/process.html.twig');
-                } catch (FileException $e) {
-                    dd('erreur upload');
-                    // Traiter l'erreur si nécessaire
-                }
-
+                $file->move($destination, $nomFichier);
+                $bus->dispatch(new AudioSplitMessage());
+                $bus->dispatch(new AudioToTextMessage());
+                return $this->render('whisper/process.html.twig');
             }
         }
 
@@ -55,16 +45,4 @@ final class WhisperController extends AbstractController
         ]);
     }
 
-    #[Route('/conversion', name: 'app_conversion')]
-    public function transcribeAudio(MessageBusInterface $bus,AudioSplitterService $audioSplitterService,  AudioConverterService $audioConverterService, WhisperService $whisperService): Response
-    {
-//        $audioConverterService->convertToMp3();
-//        $audioSplitterService->split();
-//        $whisperService->transcribeAudio();
-        $bus->dispatch(new ConvertAudioMessage());
-        $bus->dispatch(new AudioSplitMessage());
-        $bus->dispatch(new AudioToTextMessage());
-//die;
-        return $this->render('whisper/process.html.twig');
-    }
 }
